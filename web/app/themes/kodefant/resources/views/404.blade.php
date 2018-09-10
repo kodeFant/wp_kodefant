677ae2d2dcc8ce20dde21d538cf28795a9818057
kodeFant <lars.lillo@gmail.com>
2018-09-10 13:48:23 +0200

    404 page added

--- web/app/themes/kodefant/resources/views/404.blade.php
+++ web/app/themes/kodefant/resources/views/404.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
   @include('partials.page-header')
-
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+    <div class="nf404">
+      <div class="nf404-content">
+        <blockquote>{{ __('Jeg er fortapt! hvisker jeg for mig selv: mine damer og herrer, jeg er fortapt!') }}<cite><q>Sult</q> av Knut Hamsun</cite></blockquote>
+        <h1>404</h1>
+        <div class="nf404-text">
+          {{ __('Siden du prøver å finne eksisterer ikke.', 'sage') }}
+          {!! get_search_form(false) !!}
+        </div>
+      </div>
+      <img class="nf404-image" src="@asset('images/404.png')">
     </div>
-    {!! get_search_form(false) !!}
+
   @endif
 @endsection
