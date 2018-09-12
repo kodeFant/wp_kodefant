2e985bd253882f472c47669022ab3e86a63c0696
kodeFant <lars.lillo@gmail.com>
2018-09-12 14:23:49 +0200

    Small changes to byline style and added screnshot

--- web/app/themes/kodefant/resources/views/partials/entry-meta.blade.php
+++ web/app/themes/kodefant/resources/views/partials/entry-meta.blade.php
@@ -6,7 +6,7 @@
   </a>
   </div>
   <div class="post-meta post-meta-category">
-      <span class="meta-head">Kategori: </span>@include ('components/primary-category')
+      <span class="meta-head">Tema: </span>@include ('components/primary-category')
   </div>
   <div class="post-meta">
       <span class="meta-head">Publisert: </span><time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
